e51f97bdd68bc02578c53e215f239bfdafae62ef
Checho500 <56771928+checho500@users.noreply.github.com>
2021-09-08 22:01:40 -0500

    refactorizando opciones

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -3,12 +3,13 @@ import React from "react";
 import './Button.css';
 
 export default function Button(props) {
+	const id = props.id;
 	return (
 		<div className="elecciones">
-			<button id={props.id} onClick={props.onClick}>
+			<button id={id} onClick={props.onClick}>
 				Opcion {props.id.toUpperCase()}
 			</button>
-			<p>{props.opcion}</p>
+			<p>{id === 'a' ? props.opcion.a : props.opcion.b}</p>
 		</div>
 	);
 }
--- src/components/Historia/Historia.jsx
+++ src/components/Historia/Historia.jsx
@@ -12,21 +12,18 @@ export default class Historia extends Component {
 			fragmento: data[0].historia,
 			ultimaEleccion: "",
 			listaDeElecciones: [],
-			opcionA: data[0].opciones.a,
-			opcionB: data[0].opciones.b,
+			opcion: data[0].opciones
 		};
 	}
 
 	componentDidUpdate() {
-		console.log(this.state.listaDeElecciones.length);
 		if (this.state.listaDeElecciones.length === 5) {
 			alert("termino la historia, comienza de nuevo");
 			this.setState({
 				fragmento: data[0].historia,
 				ultimaEleccion: "",
 				listaDeElecciones: [],
-				opcionA: data[0].opciones.a,
-				opcionB: data[0].opciones.b
+				opcion: data[0].opciones
 			});
 		}
 	}
@@ -40,39 +37,18 @@ export default class Historia extends Component {
 		const opcion = e.target.id;
 		const paso = this.state.listaDeElecciones.length + 2;
 		if (paso !== 6) {
-			if (opcion === "a") {
-				const listaDeElecciones = [...this.state.listaDeElecciones, "A"];
-				const fragmentoElegido = this.fragmento(paso, "a").historia;
-				console.log(this.fragmento(paso, "a").id);
-				const opcionA = this.fragmento(paso, "a").opciones.a;
-				const opcionB = this.fragmento(paso, "a").opciones.b;
+			const listaDeElecciones = [...this.state.listaDeElecciones, opcion.toUpperCase()];
+			const fragmentoElegido = this.fragmento(paso, opcion).historia;
+			const opciones = this.fragmento(paso, "a").opciones;
 				this.setState({
 					listaDeElecciones: listaDeElecciones,
-					ultimaEleccion: "A",
+					ultimaEleccion: opcion.toUpperCase(),
 					fragmento: fragmentoElegido,
-					opcionA: opcionA,
-					opcionB: opcionB,
-				});
-			} else {
-				const fragmentoElegido = this.fragmento(paso, "b").historia;
-				console.log(this.fragmento(paso, "b").id);
-				const opcionA = this.fragmento(paso, "a").opciones.a;
-				const opcionB = this.fragmento(paso, "a").opciones.b;
-				const listaDeElecciones = [...this.state.listaDeElecciones, "B"];
-				this.setState({
-					listaDeElecciones: listaDeElecciones,
-					ultimaEleccion: "B",
-					fragmento: fragmentoElegido,
-					opcionA: opcionA,
-					opcionB: opcionB,
-				});
-			}
+					opcion: opciones
+			})
 		}
 		if (paso === 6) {
-			const listaDeElecciones = [
-				...this.state.listaDeElecciones,
-				opcion.toUpperCase(),
-			];
+			const listaDeElecciones = [...this.state.listaDeElecciones, opcion.toUpperCase()];
 			this.setState({
 				fragmento: "fin de la historia",
 				ultimaEleccion: opcion.toUpperCase(),
@@ -88,16 +64,8 @@ export default class Historia extends Component {
 				<div className='fragmento'>
 					<p className='textoFragmento'>{this.state.fragmento}</p>
 					<div className='opciones'>
-						<Button
-							id='a'
-							onClick={this.handleClick}
-							opcion={this.state.opcionA}
-						/>
-						<Button
-							id='b'
-							onClick={this.handleClick}
-							opcion={this.state.opcionB}
-						/>
+						<Button	id='a'	onClick={this.handleClick}	opcion={this.state.opcion}/>
+						<Button id='b' onClick={this.handleClick} opcion={this.state.opcion}/>
 					</div>
 					<Historial
 						ultimaEleccion={this.state.ultimaEleccion}
